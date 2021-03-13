53a95da0e8b2f1a4eb4a99cd06750c102a479305
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-13 13:53:43 -0600

    Updated DB, deleted items table

---
+++ db/migrate/20210313195040_delete_items_table.rb
@@ -0,0 +1,5 @@
+class DeleteItemsTable < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :items
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_194239) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.string "type"
-    t.float "amount"
-    t.integer "sale_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_03_13_195040) do
 
   create_table "sales", force: :cascade do |t|
     t.string "title"
